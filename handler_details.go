3af6ef9171f1c2fd5660cee12092ca1979eeadf7
ChaosMarc <chaosmarc@gmx.de>
2020-04-12 13:31:34 +0200

    while showing movie details a user can add/remove himself to/from the requester list when the last requester has been removed the admins are notified so they can delete the movie

--- handler_details.go
+++ handler_details.go
@@ -8,24 +8,49 @@ import (
 )
 
 func (e *Env) HandleDetails(m *tb.Message) {
+	e.CM.StartConversation(NewDetailsConversation(e), m)
+}
+
+func NewDetailsConversation(e *Env) *DetailsConversation {
+	return &DetailsConversation{env: e}
+}
+
+type DetailsConversation struct {
+	currentStep Handler
+	env         *Env
+}
+
+func (c *DetailsConversation) Run(m *tb.Message) {
+	c.currentStep = c.showDetails(m)
+}
+
+func (c *DetailsConversation) Name() string {
+	return "details"
+}
+
+func (c *DetailsConversation) CurrentStep() Handler {
+	return c.currentStep
+}
+
+func (c *DetailsConversation) showDetails(m *tb.Message) Handler {
 	movieId, err := strconv.Atoi(m.Payload)
 	if err != nil {
-		return
+		return nil
 	}
 
-	movie, err := e.Radarr.GetMovie(movieId)
+	movie, err := c.env.Radarr.GetMovie(movieId)
 	if err != nil {
-		return
+		return nil
 	}
 
 	if movie.RemotePoster == "" {
-		traktMovie, _ := e.Radarr.SearchMovie(movie.TmdbID)
-		movie.RemotePoster = e.Radarr.GetPosterURL(traktMovie)
+		traktMovie, _ := c.env.Radarr.SearchMovie(movie.TmdbID)
+		movie.RemotePoster = c.env.Radarr.GetPosterURL(traktMovie)
 	}
 
 	if movie.RemotePoster != "" {
 		photo := &tb.Photo{File: tb.FromURL(movie.RemotePoster)}
-		_, _ = e.Bot.Send(m.Sender, photo)
+		_, _ = c.env.Bot.Send(m.Sender, photo)
 	}
 
 	var msg []string
@@ -39,10 +64,62 @@ func (e *Env) HandleDetails(m *tb.Message) {
 		msg = append(msg, fmt.Sprintf("*Downloaded:* %s", FormatDateTime(movie.MovieFile.DateAdded)))
 		msg = append(msg, fmt.Sprintf("*File:* %s", movie.MovieFile.RelativePath))
 	} else {
-		msg = append(msg, fmt.Sprintf("*Downloaded:* %s", BoolToYesOrNo(movie.HasFile)))
+		msg = append(msg, "*Downloaded:* No")
 	}
-	msg = append(msg, fmt.Sprintf("*Requested by:* %s", strings.Join(e.Radarr.GetRequesterList(movie), ", ")))
+	msg = append(msg, fmt.Sprintf("*Requested by:* %s", strings.Join(c.env.Radarr.GetRequesterList(movie), ", ")))
 
-	Send(e.Bot, m.Sender, strings.Join(msg, "\n"))
+	Send(c.env.Bot, m.Sender, strings.Join(msg, "\n"))
 
+	var username string
+	if len(m.Sender.Username) > 0 {
+		username = m.Sender.Username
+	} else {
+		username = m.Sender.FirstName
+	}
+
+	var options []string
+	for _, t := range movie.Tags {
+		tag, _ := c.env.Radarr.GetTagById(t)
+		if tag.Label == strings.ToLower(username) {
+			options = append(options, "Remove yourself from the requester list")
+		}
+	}
+	if len(options) == 0 {
+		options = append(options, "Add yourself to the requester list")
+	}
+
+	options = append(options, "/cancel")
+	SendKeyboardList(c.env.Bot, m.Sender, "Modify requester list", options)
+
+	return func(m *tb.Message) {
+		for _, opt := range options {
+			if m.Text == opt {
+				if m.Text == "Add yourself to the requester list" {
+					movie, err = c.env.Radarr.AddRequester(movie, username)
+					if err != nil {
+						Send(c.env.Bot, m.Sender, "Something went wrong. Please try again.")
+					} else {
+						Send(c.env.Bot, m.Sender, "You have been added to the requester list.")
+					}
+					break
+				} else if m.Text == "Remove yourself from the requester list" {
+					movie, err = c.env.Radarr.RemoveRequester(movie, username)
+					if err != nil {
+						Send(c.env.Bot, m.Sender, "Something went wrong. Please try again.")
+					} else {
+						Send(c.env.Bot, m.Sender, "You have been removed from the requester list.")
+						if len(movie.Tags) == 0 {
+							SendAdmin(c.env.Bot, c.env.Users.Admins(),
+								fmt.Sprintf("'%s' was the last requester of the movie '%s (%d)'. Send /deletemovie\\_%d to delete it from disk.",
+									username, EscapeMarkdown(movie.Title), movie.Year, movie.ID))
+						}
+					}
+					break
+				}
+			} else {
+				SendError(c.env.Bot, m.Sender, "Invalid selection.")
+			}
+		}
+		c.env.CM.StopConversation(c)
+	}
 }
