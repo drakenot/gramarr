bbff9ee5d01a7eb7a969f97b76b81b0ec03c701a
ChaosMarc <chaosmarc@gmx.de>
2020-05-01 12:17:08 +0200

    removeRequester option in /moviedetails for admins WIP

--- handler_details.go
+++ handler_details.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gramarr/radarr"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"strconv"
 	"strings"
@@ -18,6 +19,7 @@ func NewDetailsConversation(e *Env) *DetailsConversation {
 type DetailsConversation struct {
 	currentStep Handler
 	env         *Env
+	movie       radarr.Movie
 }
 
 func (c *DetailsConversation) Run(m *tb.Message) {
@@ -38,35 +40,35 @@ func (c *DetailsConversation) showDetails(m *tb.Message) Handler {
 		return nil
 	}
 
-	movie, err := c.env.Radarr.GetMovie(movieId)
+	c.movie, err = c.env.Radarr.GetMovie(movieId)
 	if err != nil {
 		return nil
 	}
 
-	if movie.RemotePoster == "" {
-		traktMovie, _ := c.env.Radarr.SearchMovie(movie.TmdbID)
-		movie.RemotePoster = c.env.Radarr.GetPosterURL(traktMovie)
+	if c.movie.RemotePoster == "" {
+		traktMovie, _ := c.env.Radarr.SearchMovie(c.movie.TmdbID)
+		c.movie.RemotePoster = c.env.Radarr.GetPosterURL(traktMovie)
 	}
 
-	if movie.RemotePoster != "" {
-		photo := &tb.Photo{File: tb.FromURL(movie.RemotePoster)}
+	if c.movie.RemotePoster != "" {
+		photo := &tb.Photo{File: tb.FromURL(c.movie.RemotePoster)}
 		_, _ = c.env.Bot.Send(m.Sender, photo)
 	}
 
 	var msg []string
-	msg = append(msg, fmt.Sprintf("*%s (%d)*", EscapeMarkdown(movie.Title), movie.Year))
-	msg = append(msg, movie.Overview)
+	msg = append(msg, fmt.Sprintf("*%s (%d)*", EscapeMarkdown(c.movie.Title), c.movie.Year))
+	msg = append(msg, c.movie.Overview)
 	msg = append(msg, "")
-	msg = append(msg, fmt.Sprintf("*Cinema Date:* %s", FormatDate(movie.InCinemas)))
-	msg = append(msg, fmt.Sprintf("*BluRay Date:* %s", FormatDate(movie.PhysicalRelease)))
-	msg = append(msg, fmt.Sprintf("*Folder:* %s", GetRootFolderFromPath(movie.Path)))
-	if movie.HasFile {
-		msg = append(msg, fmt.Sprintf("*Downloaded:* %s", FormatDateTime(movie.MovieFile.DateAdded)))
-		msg = append(msg, fmt.Sprintf("*File:* %s", movie.MovieFile.RelativePath))
+	msg = append(msg, fmt.Sprintf("*Cinema Date:* %s", FormatDate(c.movie.InCinemas)))
+	msg = append(msg, fmt.Sprintf("*BluRay Date:* %s", FormatDate(c.movie.PhysicalRelease)))
+	msg = append(msg, fmt.Sprintf("*Folder:* %s", GetRootFolderFromPath(c.movie.Path)))
+	if c.movie.HasFile {
+		msg = append(msg, fmt.Sprintf("*Downloaded:* %s", FormatDateTime(c.movie.MovieFile.DateAdded)))
+		msg = append(msg, fmt.Sprintf("*File:* %s", c.movie.MovieFile.RelativePath))
 	} else {
 		msg = append(msg, "*Downloaded:* No")
 	}
-	requesterList := c.env.Radarr.GetRequesterList(movie)
+	requesterList := c.env.Radarr.GetRequesterList(c.movie)
 	msg = append(msg, fmt.Sprintf("*Requested by:* %s", strings.Join(requesterList, ", ")))
 
 	Send(c.env.Bot, m.Sender, strings.Join(msg, "\n"))
@@ -79,23 +81,23 @@ func (c *DetailsConversation) showDetails(m *tb.Message) Handler {
 	}
 
 	var options []string
-	for _, t := range movie.Tags {
-		tag, _ := c.env.Radarr.GetTagById(t)
-		if tag.Label == strings.ToLower(username) {
-			options = append(options, "Remove yourself from the requester list")
-			break
-		}
-	}
-	if len(options) == 0 {
-		options = append(options, "Add yourself to the requester list")
-	}
-
 	user, exists := c.env.Users.User(m.Sender.ID)
 	if exists && user.IsAdmin() {
+		if len(c.movie.Tags) > 0 {
+			options = append(options, "Remove requester")
+		}
 		options = append(options, "/deletemovie_"+m.Payload)
-		//for requester := range requesterList {
-		//	options = append(options, "/removerequester_" + requester)
-		//}
+	} else {
+		for _, t := range c.movie.Tags {
+			tag, _ := c.env.Radarr.GetTagById(t)
+			if tag.Label == strings.ToLower(username) {
+				options = append(options, "Remove yourself from the requester list")
+				break
+			}
+		}
+		if len(options) == 0 {
+			options = append(options, "Add yourself to the requester list")
+		}
 	}
 
 	options = append(options, "/cancel")
@@ -105,7 +107,7 @@ func (c *DetailsConversation) showDetails(m *tb.Message) Handler {
 		for _, opt := range options {
 			if m.Text == opt {
 				if m.Text == "Add yourself to the requester list" {
-					movie, err = c.env.Radarr.AddRequester(movie, username)
+					c.movie, err = c.env.Radarr.AddRequester(c.movie, username)
 					if err != nil {
 						Send(c.env.Bot, m.Sender, "Something went wrong. Please try again.")
 					} else {
@@ -113,23 +115,46 @@ func (c *DetailsConversation) showDetails(m *tb.Message) Handler {
 					}
 					break
 				} else if m.Text == "Remove yourself from the requester list" {
-					movie, err = c.env.Radarr.RemoveRequester(movie, username)
-					if err != nil {
-						Send(c.env.Bot, m.Sender, "Something went wrong. Please try again.")
-					} else {
-						Send(c.env.Bot, m.Sender, "You have been removed from the requester list.")
-						if len(movie.Tags) == 0 {
-							SendAdmin(c.env.Bot, c.env.Users.Admins(),
-								fmt.Sprintf("'%s' was the last requester of the movie '%s (%d)'. Send /deletemovie\\_%d to delete it from disk.",
-									username, EscapeMarkdown(movie.Title), movie.Year, movie.ID))
-						}
-					}
+					c.removeRequester(m, username)
 					break
+				} else if m.Text == "Remove requester" {
+
 				}
 			} else {
 				SendError(c.env.Bot, m.Sender, "Invalid selection.")
 			}
 		}
 		c.env.CM.StopConversation(c)
+	}
+}
+
+func (c *DetailsConversation) askRemoveRequester(m *tb.Message) Handler {
+	Send(c.env.Bot, m.Sender, "Remove user from requester list")
+
+	var options []string
+	for _, t := range c.movie.Tags {
+		tag, _ := c.env.Radarr.GetTagById(t)
+		options = append(options, fmt.Sprintf("Remove %s from the requester list"), tag.Label)
+	}
+	options = append(options, "/cancel")
+	SendKeyboardList(c.env.Bot, m.Sender, "Modify requester list", options)
+
+	return func(m *tb.Message) {
+		c.removeRequester(m, m.Text)
+	}
+}
+
+func (c *DetailsConversation) removeRequester(m *tb.Message, requester string) {
+	var err error
+	c.movie, err = c.env.Radarr.RemoveRequester(c.movie, requester)
+	if err == nil {
+		Send(c.env.Bot, m.Sender, fmt.Sprintf("%s has been removed from the requester list.", requester))
+		if len(c.movie.Tags) == 0 {
+			SendAdmin(c.env.Bot, c.env.Users.Admins(),
+				fmt.Sprintf("'%s' was the last requester of the movie '%s (%d)'. Send /deletemovie\\_%d to delete it from disk.",
+					requester, EscapeMarkdown(c.movie.Title), c.movie.Year, c.movie.ID))
+		}
+	} else {
+		Send(c.env.Bot, m.Sender, "Something went wrong. Please try again.")
 	}
 }
