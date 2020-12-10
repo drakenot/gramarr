93576353f17cbe1214a47a8edb39881927a21250
Shiv Adhiappan <shivaram1190@gmail.com>
2020-12-09 21:46:49 -0600

    Season folder fix

--- sonarr/config.go
+++ sonarr/config.go
@@ -1,13 +1,14 @@
 package sonarr
 
 type Config struct {
-	Hostname   string `json:"hostname"`
-	APIKey     string `json:"apiKey"`
-	Port       int    `json:"port"`
-	URLBase    string `json:"urlBase"`
-	SSL        bool   `json:"ssl"`
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	MaxResults int    `json:"maxResults"`
-	QualityID  int    `json:"qualityId"`
+	Hostname   	string `json:"hostname"`
+	APIKey     	string `json:"apiKey"`
+	Port       	int    `json:"port"`
+	URLBase    	string `json:"urlBase"`
+	SSL        	bool   `json:"ssl"`
+	Username   	string `json:"username"`
+	Password   	string `json:"password"`
+	MaxResults 	int    `json:"maxResults"`
+	QualityID  	int    `json:"qualityId"`
+	SeasonFolder	bool   `json:"seasonFolder"`
 }
